162833a26a93f9e95e4428002b94736272183257
ned1313 <ned.bellavance@gmail.com>
2021-06-14 16:58:37 -0400

    m2 working

--- m2/create-peering.sh
+++ m2/create-peering.sh
@@ -7,52 +7,41 @@ region=us-east-1
 
 export AWS_DEFAULT_REGION=$region
 
-# Create the CloudFormation stack for the basic VPC
-aws cloudformation create-stack --stack-name Globomantics2 --template-body file://basic-network-2.json
+# Create the CloudFormation stack for the secondary VPC and peering
+aws ec2 create-key-pair --key-name Peering --query 'KeyMaterial' --output text > Peering.pem
 
-# Once the VPC is in place, we'll create the peering connections
-# We'll need some information
+account_id=$(aws ec2 describe-vpcs --filters Name="tag:Name",Values="globo-primary" \
+  --query 'Vpcs[0].OwnerId' --output text)
 
 vpc1_id=$(aws ec2 describe-vpcs --filters Name="tag:Name",Values="globo-primary" \
   --query 'Vpcs[0].VpcId' --output text)
 
-vpc2_id=$(aws ec2 describe-vpcs --filters Name="tag:Name",Values="globo-secondary" \
-  --query 'Vpcs[0].VpcId' --output text)
-
-vpc1_routetable_ids
-
-# Create a key-pair for your instance
-
-aws ec2 create-key-pair --key-name Trouble --query 'KeyMaterial' --output text > Trouble.pem
-
-# Create a security group for your web instance
-
-aws ec2 create-security-group --description "default-sg-for-Trouble" \
-  --group-name "Trouble" --vpc-id $vpc_id
-
-sg_id=$(aws ec2 describe-security-groups --filter Name="group-name",Values="Trouble" \
-  --query 'SecurityGroups[0].GroupId' --output text)
-
-# Get the latest Amazon Linux 2 AMI 
-
 ami_id=$(aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 \
   --region $region | jq .Parameters[].Value -r)
 
-# Spin up the web-1 instance
+aws cloudformation create-stack --stack-name Globomantics2 \
+  --template-body file://basic-network-2.json \
+  --parameters ParameterKey=PeerName,ParameterValue="globo-peering" \
+  ParameterKey=PeerOwnerId,ParameterValue="$account_id" \
+  ParameterKey=PeerVPCID,ParameterValue="$vpc1_id" \
+  ParameterKey=PeerVPCCIDR,ParameterValue="10.0.0.0/16" \
+  ParameterKey=ImageId,ParameterValue="$ami_id"
 
-aws ec2 run-instances --image-id $ami_id --count 1 \
-  --instance-type t2.micro --key-name Trouble \
-  --security-group-ids $sg_id --subnet-id $subnet1_id \
-  --user-data file://install-web-server.txt \
-  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=web-1},{Key=Company,Value=Globomantics}]' \
-  'ResourceType=volume,Tags=[{Key=Name,Value=web-1},{Key=Company,Value=Globomantics}]'
+# Get the public IP for SSH
+public_ip_address=$(aws ec2 describe-instances --filters Name="tag:Name",Values="vpc2-peering" \
+  --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
-web_url=$(aws ec2 describe-instances --filters Name="tag:Name",Values="web-1" \
-  --query 'Reservations[0].Instances[0].PublicDnsName' --output text)
-
-curl http://$web_url
-
-dig $web_url
-
+# Get the private IP for the web instance
 aws ec2 describe-instances --filters Name="tag:Name",Values="web-1" \
-  --query 'Reservations[0].Instances[0].PublicIpAddress' --output text
+  --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text
+
+# Copy your ssh key and update permissions
+cp Peering.pem ~/.ssh/
+chmod 600 ~/.ssh/Peering.pem
+
+# Connect to the vpc2-peering instance
+ssh ec2-user@$public_ip_address -i ~/.ssh/Peering.pem
+
+# Try to curl to the private IP address of web-1
+
+# Use the reachability analyzer in the Console to troubleshoot the issue
