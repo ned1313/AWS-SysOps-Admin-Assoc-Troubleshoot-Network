2bb3e5fec10fa822ab991acdc13f0fc7dcf1d988
ned1313 <ned.bellavance@gmail.com>
2021-06-16 19:37:33 -0400

    add transit gateway demo

---
+++ m2/create-transit-gateway.sh
@@ -0,0 +1,19 @@
+# Deploy our transit gateway setup
+
+aws cloudformation create-stack --stack-name TransitGateway \
+  --template-body file://transit-gateway.json \
+  --parameters ParameterKey=ImageId,ParameterValue="$ami_id" \
+  ParameterKey=KeyName,ParameterValue="Peering"
+
+# Get the public IP address of one of the mgmt instance
+
+ public_ip_address=$(aws ec2 describe-instances --filters Name="tag:Name",Values="mgmt-transit-test" \
+  --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+
+# Get the private IP address of the App1 instance
+aws ec2 describe-instances --filters Name="tag:Name",Values="app1-transit-test" \
+  --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text
+
+# Connect via SSH
+
+ssh ec2-user@$public_ip_address -i ~/.ssh/Peering.pem
